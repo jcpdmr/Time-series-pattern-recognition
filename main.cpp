5e68131f6e82ebcf4ce3e84c0fa8c2138432f027
Jacopo Damerini <76707805+jcpdmr@users.noreply.github.com>
2024-02-05 13:02:47 +0100

    Added load of CSV file

--- main.cpp
+++ main.cpp
@@ -1,23 +1,55 @@
-// OpenMP header
 #include "utility.h"
 
-int main(int argc, char* argv[])
-{
-	int nthreads, tid;
+int main() {
+    // Open the file
+    ifstream file("input_data/NVDA.csv");
+    if (!file.is_open()) {
+        cerr << "Error opening the file!" << endl;
+        return 1;
+    }
 
-	// Begin of parallel region
-	#pragma omp parallel private(nthreads, tid)
-	{
-		// Getting thread number
-		tid = omp_get_thread_num();
-		printf("Welcome to GFG from thread = %d\n",
-			tid);
+    vector<tm> dates;
+    vector<float> values;
+	dates.reserve(SERIES_LENGHT);
+	values.reserve(SERIES_LENGHT);
 
-		if (tid == 0) {
+    string line;
 
-			// Only master thread does this
-			nthreads = omp_get_num_threads();
-			test(nthreads);
-		}
-	}
+	// Skip first line (because it contains the header row)
+	getline(file, line);
+
+    while (getline(file, line)) {
+        istringstream line_stream(line);
+        string token;
+        vector<string> tokens;
+		tokens.reserve(7 * SERIES_LENGHT);
+
+        while (getline(line_stream, token, ',')) {
+            tokens.push_back(token);
+        }
+        if (tokens.size() >= 6) {
+			// Get date and close value
+            tm date = {};
+            istringstream date_stream(tokens[0]);
+            date_stream >> get_time(&date, "%Y-%m-%d");
+            dates.push_back(date);
+            values.push_back(stof(tokens[4])); 
+        }
+    }
+
+    // Stampa le date e i valori
+    cout << "Dates:" << endl;
+    for (const auto& date : dates) {
+        cout << put_time(&date, "%Y-%m-%d") << endl;
+    }
+
+    cout << "Values:" << endl;
+    for (const auto& value : values) {
+        cout << value << endl;
+    }
+
+    // Chiudi il file
+    file.close();
+
+    return 0;
 }
--- utility.h
+++ utility.h
@@ -1,10 +1,22 @@
-#ifndef DES_DECRYPTION_UTILITY_H
-#define DES_DECRYPTION_UTILITY_H
+#ifndef TIME_SERIES_PATTERN_RECOGNITION_UTILITY_H
+#define TIME_SERIES_PATTERN_RECOGNITION_UTILITY_H
 
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+#define SERIES_LENGHT 1300
+
+using namespace std;
+
+
 
 void test(int n);
 
-#endif //DES_DECRYPTION_UTILITY_H
+#endif //TIME_SERIES_PATTERN_RECOGNITION_UTILITY_H
