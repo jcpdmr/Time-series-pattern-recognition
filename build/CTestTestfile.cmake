98dd7f9924f349d2bc0346d0e659ea31e36e7a3e
Jacopo Damerini <76707805+jcpdmr@users.noreply.github.com>
2024-02-05 12:13:41 +0100

    Delete directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bleffjack/Desktop/Time-series-pattern-recognition
-# Build directory: /home/bleffjack/Desktop/Time-series-pattern-recognition/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
