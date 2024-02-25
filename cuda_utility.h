4e997ecf8ca03fcd1e4073afe43218c676d2ce29
Jacopo Damerini <76707805+jcpdmr@users.noreply.github.com>
2024-02-25 23:35:34 +0100

    Problem found

--- cuda_utility.h
+++ cuda_utility.h
@@ -12,8 +12,11 @@
 #include <iomanip>
 #include <cmath>
 #include <chrono>
+#include <map>
 
-#define SERIES_LENGTH 52560
+#define SERIES_LENGTH 2075260
+#define N_FILTERS 4
+#define FILTER_LENGTH 10080
 #define checkCudaErrors(result) { checkCuda(result, __FILE__, __LINE__); }
 
 using namespace std;
@@ -39,36 +42,102 @@ __global__ void calculate_means_windowed(const float* values, float* means, int 
     }
 }
 
-__global__ void calculate_stds_windowed(const float* values, const float* means, float* stds, int window_size, int series_length) {
+__global__ void calculate_stds_znccs_windowed(const float* values, const float* means, float* stds, float* znccs, float* filter, float* filter_mean, float* filter_std, int window_size, int series_length) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     // Only the threads that work with a window that completely covers the data can compute 
     if ((idx >= window_size / 2) && (idx < series_length - window_size / 2)) {
+        // Compute std
         float variance_summation = 0.0f;
         for (int i = idx - window_size / 2; i <= idx + window_size / 2; ++i) {
             variance_summation += pow(values[i] - means[idx], 2);
         }
-        stds[idx] = sqrt(variance_summation / (window_size - 1)) ;
+        stds[idx] = sqrt(variance_summation / (window_size - 1));
+
+        // Compute zncc
+        float zncc_sum = 0.0f;
+        for (int i = idx - window_size / 2; i <= idx + window_size / 2; ++i) {
+            zncc_sum += (values[i] - means[idx]) * (filter[i - idx + window_size / 2] - (*filter_mean));
+        }
+        znccs[idx] = (zncc_sum / (window_size * stds[idx] * (*filter_std)));
     }
 }
 
-__global__ void calculate_znccs_windowed(const float* values, const float* means, float* stds, float* zncc, float* filter, float filter_mean, float filter_std, int window_size, int series_length) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void calculate_SADs(const float* values, float* SADs, float* filter, int window_size, int series_length, int filter_idx) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x + (window_size / 2);
     // Only the threads that work with a window that completely covers the data can compute 
-    if ((idx >= window_size / 2) && (idx < series_length - window_size / 2)) {
-        float zncc_sum = 0.0f;
+    if (idx < series_length - window_size / 2){
+        float SAD = 0;     
         for (int i = idx - window_size / 2; i <= idx + window_size / 2; ++i) {
-            zncc_sum += (values[i] - means[idx]) * (filter[i - idx + window_size / 2] - filter_mean);
-            // if (idx == 5){
-            //     printf("[%i] Val: %f  FVal: %f\n", i, values[i], filter[i - idx + window_size / 2]);
-            //     printf("[%i] V - M: %f   F - M: %f   Curr: %f   Sum: %f \n", i, (values[i] - means[idx]), (filter[i - idx + window_size / 2] - filter_mean) ,(values[i] - means[idx]) * (filter[i - idx + window_size / 2] - filter_mean), zncc_sum);
-            // }
+            SAD += abs(values[i] - filter[i - idx + window_size / 2]);
         }
-        zncc[idx] = (zncc_sum / (window_size * stds[idx] * filter_std));
-        if(idx < 10 && idx > 4){
-            printf("[%i] std: %f \n", idx, stds[idx]);
-            // printf("[%i] zncc: %f", idx, zncc[idx]);
+        SADs[series_length * filter_idx + idx] = SAD / window_size;
+    }
+}
+
+vector<float> create_filter_trend_n_weeks(const int n_of_weeks, bool uptrend=true){
+    vector<float>filt;
+    filt.reserve(n_of_weeks * 1440 * 7);
+    if(uptrend){
+        float val = 1.0f;
+        for(int i = 0; i < n_of_weeks * 1440 * 7; i++){
+            filt.push_back(val);
+            val++;
         }
     }
+    else{
+        float val = -1.0f;
+        for(int i = 0; i < n_of_weeks * 1440 * 7; i++){
+            filt.push_back(val);
+            val--;
+        }
+    }
+
+    return filt;
+}
+
+
+vector<float> create_filter_cycle_n_weeks(const int n_of_weeks, bool up_then_down){
+
+    if((n_of_weeks % 2) != 0){
+    cerr << "Number of weeks must be divisible by two" << endl;
+    // Return an empty vector to indicate the error
+    return vector<float>();
+    }
+
+    vector<float>filt;
+    filt.reserve(n_of_weeks * 1440 * 7);
+
+    int middle_point = (n_of_weeks / 2) * 1440 * 7;
+    if(up_then_down){
+        // Add the first half of upgoing values
+        float val = 1.0f;
+        for(int i = 0; i < middle_point; i++){
+            filt.push_back(val);
+            val++;
+        }
+        // Add the second half of downgoing values
+        val--;
+        for(int i = middle_point; i < n_of_weeks * 1440 * 7; i++){
+            filt.push_back(val);
+            val--;
+        }
+    }
+    else{
+        // Add the first half of upgoing values
+        float val = -1.0f;
+        for(int i = 0; i < middle_point; i++){
+            filt.push_back(val);
+            val--;
+        }
+        // Add the second half of downgoing values
+        val++;
+        for(int i = middle_point; i < n_of_weeks * 1440 * 7; i++){
+            filt.push_back(val);
+            val++;
+        }
+    }
+
+    return filt;
 }
 
 #endif //TIME_SERIES_PATTERN_RECOGNITION_CUDA_UTILITY_H
--- main.cu
+++ main.cu
@@ -2,7 +2,7 @@
 
 int main() {
     // Open the file
-    ifstream file("../input_data/household_power_consumption_short.txt");
+    ifstream file("../input_data/household_power_consumption.txt");
     if (!file.is_open()) {
         cerr << "Error opening the file!" << endl;
         return 1;
@@ -42,71 +42,170 @@ int main() {
     // Remeber to close the file
     file.close();
 
-    const int window_size = 11;
+    vector<vector<float>> temp_filters;
+    temp_filters.push_back(create_filter_trend_n_weeks(1, true));
+    temp_filters.push_back(create_filter_trend_n_weeks(1, true));
+    temp_filters.push_back(create_filter_trend_n_weeks(1, true));
+    temp_filters.push_back(create_filter_trend_n_weeks(1, true));
+    temp_filters.push_back(create_filter_trend_n_weeks(1, true));
+    temp_filters.push_back(create_filter_trend_n_weeks(1, true));
+    temp_filters.push_back(create_filter_trend_n_weeks(1, true));
+    temp_filters.push_back(create_filter_trend_n_weeks(1, true));
+    // Create a bank of filters
+    const vector<vector<float>> filters = temp_filters;
+
+    auto start_benchmark = chrono::high_resolution_clock::now();
+    
     const int block_size = 256;
-    const int n_blocks = (SERIES_LENGTH + block_size - 1) / block_size;
 
-    const vector<float> filter = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f};
+    // Allocate CPU space for the result
+    vector<float> SADs(filters.size() * SERIES_LENGTH, 0.0f);
 
-    float *d_values, *d_means, *d_stds, *d_znccs, *d_filter;
+    // Allocate GPU space for data values, results and filters
+    float *d_values, *d_SADs, *d_filters;
     checkCudaErrors(cudaMalloc((void**)&d_values, SERIES_LENGTH * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void**)&d_means, SERIES_LENGTH * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void**)&d_stds, SERIES_LENGTH * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void**)&d_filter, filter.size() * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void**)&d_znccs, SERIES_LENGTH * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void**)&d_SADs, filters.size() * SERIES_LENGTH * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void**)&d_filters, filters.size() * FILTER_LENGTH * sizeof(float)));
 
-    // Copying data from CPU to GPU
+    // Copying data values and filters from CPU to GPU
     checkCudaErrors(cudaMemcpy(d_values, values.data(), SERIES_LENGTH * sizeof(float), cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMemcpy(d_filter, filter.data(), filter.size() * sizeof(float), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_filters , filters.data(), filters.size() * FILTER_LENGTH * sizeof(float), cudaMemcpyHostToDevice));
 
-    calculate_means_windowed<<<n_blocks, block_size>>>(d_values, d_means, window_size, SERIES_LENGTH);
-    checkCudaErrors(cudaGetLastError());
-    // Wait for the kernel execution to finish
+    for (int query_idx = 0; query_idx < filters.size(); query_idx++){
+        const vector<float> query = filters[query_idx];
+        const int window_size = query.size();
+        
+        // checkCudaErrors(cudaMemcpy(d_filters + query_idx * query.size(), query.data(), query.size() * sizeof(float), cudaMemcpyHostToDevice));
+        
+        const int n_blocks = (SERIES_LENGTH - window_size + (block_size - 1)) / block_size;
+        calculate_SADs<<<n_blocks, block_size>>>(d_values, d_SADs, d_filters, window_size, SERIES_LENGTH, query_idx);
+        checkCudaErrors(cudaGetLastError());
+
+    }
+
+    // Wait for all kernels execution to finish
     checkCudaErrors(cudaDeviceSynchronize());
-
-    calculate_stds_windowed<<<n_blocks, block_size>>>(d_values, d_means, d_stds, window_size, SERIES_LENGTH);
-    checkCudaErrors(cudaGetLastError());
-    // Wait for the kernel execution to finish
-    checkCudaErrors(cudaDeviceSynchronize());
-
-    calculate_znccs_windowed<<<n_blocks, block_size>>>(d_values, d_means, d_stds, d_znccs, d_filter, 6.0f, 3.3166247903554, window_size, SERIES_LENGTH);
-    checkCudaErrors(cudaGetLastError());
-    // Wait for the kernel execution to finish
-    checkCudaErrors(cudaDeviceSynchronize());
-
     // Copy results from GPU to CPU
-    vector<float> means(SERIES_LENGTH, 0.0f);
-    vector<float> stds(SERIES_LENGTH, 0.0f);
-    vector<float> znccs(SERIES_LENGTH, 0.0f);
-    checkCudaErrors(cudaMemcpy(means.data(), d_means, SERIES_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
-    checkCudaErrors(cudaMemcpy(stds.data(), d_stds, SERIES_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
-    checkCudaErrors(cudaMemcpy(znccs.data(), d_znccs, SERIES_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(SADs.data(), d_SADs, filters.size() * SERIES_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
 
-    // Free resources
+    // Free all resources
+    cudaFree(d_filters);
     cudaFree(d_values);
-    cudaFree(d_means);
-    cudaFree(d_stds);
-    cudaFree(d_znccs);
-    cudaFree(d_filter);
 
-    // Print results
-    // for (int i = 0; i < SERIES_LENGTH; ++i) {
-    //     if ((i < window_size + 5) || (i > SERIES_LENGTH - window_size - 5)){
-    //         cout << "Means at index " << i << ": " << means[i] << endl;
+
+    auto stop_benchmark = chrono::high_resolution_clock::now();
+    auto duration_benchmark = chrono::duration_cast<chrono::milliseconds >(stop_benchmark - start_benchmark).count();
+    
+    cout << "Benchmark elapsed time: " << duration_benchmark << " ms" << endl;
+
+    // Save data
+    #pragma omp parallel for
+    for(int query_idx = 0; query_idx < filters.size(); query_idx++){
+        const vector<float> query = filters[query_idx];
+        const int window_size = query.size();
+
+        string file_name = "../output_data/SAD"+ to_string(query_idx) + "_filterlen" + to_string(query.size()) +".txt";
+        ofstream output_file(file_name);
+        if (output_file.is_open()) {
+            for (int i = query_idx * SERIES_LENGTH; i < (query_idx + 1) * SERIES_LENGTH; i++){
+                output_file << SADs[i] << "\n";
+            }
+            output_file.close();
+            cout << "Saved successfully: " << file_name << endl;
+        } else {
+            cerr << "Failed to open: " << file_name << endl;
+        }
+    }
+
+    // // Allocate space for the results
+    // vector<float> means(SERIES_LENGTH, 0.0f);
+    // vector<float> stds(SERIES_LENGTH, 0.0f);
+    // map<int, vector<float>> znccs;
+
+    // // Allocate GPU memory
+    // float *d_values, *d_means, *d_stds, *d_znccs, *d_filter, *d_filt_mean, *d_filt_std;
+    // checkCudaErrors(cudaMalloc((void**)&d_values, SERIES_LENGTH * sizeof(float)));
+    // checkCudaErrors(cudaMalloc((void**)&d_means, SERIES_LENGTH * sizeof(float)));
+    // checkCudaErrors(cudaMalloc((void**)&d_stds, SERIES_LENGTH * sizeof(float)));
+    // checkCudaErrors(cudaMalloc((void**)&d_znccs, SERIES_LENGTH * sizeof(float)));
+    // checkCudaErrors(cudaMalloc((void**)&d_filt_mean, 1 * sizeof(float)));
+    // checkCudaErrors(cudaMalloc((void**)&d_filt_std, 1 * sizeof(float)));
+    // // Copying data from CPU to GPU
+    // checkCudaErrors(cudaMemcpy(d_values, values.data(), SERIES_LENGTH * sizeof(float), cudaMemcpyHostToDevice));
+
+    // for (int query_idx = 0; query_idx < filters.size(); query_idx++){
+    //     const vector<float> query = filters[query_idx];
+    //     const int window_size = query.size();
+    //     float filt_mean, filt_std;
+
+    //     // Calculate mean and std of filter
+    //     float sum = 0.0f;
+    //     for (int i = 0; i <= window_size; ++i) {
+    //         sum += query[i];
     //     }
-    // }
-    // for (int i = 0; i < SERIES_LENGTH; ++i) {
-    //     if ((i < window_size + 5) || (i > SERIES_LENGTH - window_size - 5)){
-    //         cout << "Std at index " << i << ": " << stds[i] << endl;
+    //     filt_mean = sum / window_size;
+
+    //     float variance_summation = 0.0f;
+    //     for (int i = 0; i < window_size; ++i) {
+    //         variance_summation += pow(query[i] - filt_mean, 2);
     //     }
+    //     filt_std = sqrt(variance_summation / (window_size - 1));
+
+    //     // Allocate and copy the filter values
+    //     checkCudaErrors(cudaMalloc((void**)&d_filter, query.size() * sizeof(float)));
+    //     checkCudaErrors(cudaMemcpy(d_filter, query.data(), query.size() * sizeof(float), cudaMemcpyHostToDevice));
+    //     checkCudaErrors(cudaMemcpy(d_filt_mean, &filt_mean, 1 * sizeof(float), cudaMemcpyHostToDevice));
+    //     checkCudaErrors(cudaMemcpy(d_filt_std, &filt_std, 1 * sizeof(float), cudaMemcpyHostToDevice));
+
+    //     calculate_means_windowed<<<n_blocks, block_size>>>(d_values, d_means, window_size, SERIES_LENGTH);
+    //     checkCudaErrors(cudaGetLastError());
+    //     // Wait for the kernel execution to finish
+    //     checkCudaErrors(cudaDeviceSynchronize());
+
+    //     calculate_stds_znccs_windowed<<<n_blocks, block_size>>>(d_values, d_means, d_stds, d_znccs, d_filter, d_filt_mean, d_filt_std, window_size, SERIES_LENGTH);
+    //     checkCudaErrors(cudaGetLastError());
+    //     // Wait for the kernel execution to finish
+    //     checkCudaErrors(cudaDeviceSynchronize());
+
+
+    //     // Copy results from GPU to CPU
+    //     checkCudaErrors(cudaMemcpy(means.data(), d_means, SERIES_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
+    //     checkCudaErrors(cudaMemcpy(stds.data(), d_stds, SERIES_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
+    //     znccs[window_size] = vector<float>(SERIES_LENGTH, 0.0f);
+    //     checkCudaErrors(cudaMemcpy(znccs[window_size].data(), d_znccs, SERIES_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
+        
+    //     // Free space of the filter
+    //     cudaFree(d_filter);
     // }
-    // for (int i = 0; i < SERIES_LENGTH; ++i) {
-    //     if ((i < window_size + 5) || (i > SERIES_LENGTH - window_size - 5)){
-    //         cout << "Zncc at index " << i << ": " << znccs[i] << endl;
+   
+
+    // // Free all other resources
+    // cudaFree(d_values);
+    // cudaFree(d_means);
+    // cudaFree(d_stds);
+    // cudaFree(d_znccs);
+    // cudaFree(d_filt_mean);
+    // cudaFree(d_filt_std);
+
+    // // Save data
+    // #pragma omp parallel for
+    // for(int query_idx = 0; query_idx < filters.size(); query_idx++){
+    //     const vector<float> query = filters[query_idx];
+    //     const int window_size = query.size();
+
+    //     string file_name = "../output_data/zn_cross_correlation"+ to_string(query_idx) + "_filterlen" + to_string(query.size()) +".txt";
+    //     ofstream output_file(file_name);
+    //     if (output_file.is_open()) {
+    //         for (float value : znccs[window_size]) {
+    //             output_file << value << "\n";
+    //         }
+    //         output_file.close();
+    //         cout << "Saved successfully: " << file_name << endl;
+    //     } else {
+    //         cerr << "Failed to open: " << file_name << endl;
     //     }
     // }
 
-    cout << "Finished" << endl;
 
     return 0;
 }
